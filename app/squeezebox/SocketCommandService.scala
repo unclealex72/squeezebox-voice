6c0b946f9c48c091d8f3c9f49dd93553ae07b885
Alex Jones <alex.jones@unclealex.co.uk>
2018-06-23 14:32:54 +0100

    Add logging to the socket command service.

--- app/squeezebox/SocketCommandService.scala
+++ app/squeezebox/SocketCommandService.scala
@@ -3,12 +3,18 @@ import java.io._
 import java.net.Socket
 import java.nio.charset.StandardCharsets
 
+import com.typesafe.scalalogging.StrictLogging
+
 import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Created by alex on 23/12/17
   **/
-class SocketCommandService(squeezeCentreLocation: SqueezeCentreLocation)(implicit ec: ExecutionContext) extends CommandService {
+class SocketCommandService(squeezeCentreLocation: SqueezeCentreLocation)(implicit ec: ExecutionContext) extends CommandService with StrictLogging {
+
+  val port: Int = squeezeCentreLocation.port
+  val host: String = squeezeCentreLocation.host
+
   /**
     * Execute a command on the logitech media server.
     *
@@ -16,7 +22,8 @@ class SocketCommandService(squeezeCentreLocation: SqueezeCentreLocation)(implici
     * @return The eventual return value of the command.
     */
   override def execute(command: String): Future[String] = Future {
-    val socket = new Socket(squeezeCentreLocation.host, squeezeCentreLocation.port)
+    logger.info(s"Sending command to $host:$port - $command")
+    val socket = new Socket(host, port)
     try {
       val writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream, StandardCharsets.UTF_8.name()))
       writer.write(command)
